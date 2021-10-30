ebb47f35efd736aecef769cd85957c7b3af1cf6b
Jan Gru <j4n6ru@gmail.com>
2021-10-30 16:54:49 +0200

    Add reverse turing tests
    Add reverse Turing tests, which mimick checks found in real world
    samples like the UpClicker trojan, leaked source code of Ursnif/Gozi,
    a "DarkRiver" dropper, the MyWeb-backdoor and XLM 4.0-macros used as
    droppers.

--- pafish/gensandbox.h
+++ pafish/gensandbox.h
@@ -2,8 +2,6 @@
 #ifndef GENSAND_H
 #define GENSAND_H
 
-int gensandbox_mouse_act();
-
 int gensandbox_username();
 
 int gensandbox_path();
---
+++ pafish/rtt.h
@@ -0,0 +1,18 @@
+#ifndef RTT_H
+#define RTT_H
+
+int rtt_mouse_presence();
+
+int rtt_mouse_move();
+
+int rtt_mouse_speed_limit();
+
+int rtt_mouse_click();
+
+int rtt_mouse_double_click();
+
+int rtt_confirm_dialog();
+
+int rtt_plausible_confirm_dialog();
+
+#endif
